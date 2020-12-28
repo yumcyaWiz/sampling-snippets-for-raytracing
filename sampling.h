941b7f15199ad4ffcd8325670c42390c4dfab523
yumcyawiz <hashdolf1923@gmail.com>
2020-12-29 03:21:31 +0900

    fix pdf of sampleCircle

--- sampling.h
+++ sampling.h
@@ -8,7 +8,7 @@
 
 // sample point on circle
 inline Vec2 sampleCircle(float u, float v, float R, float& pdf) {
-  pdf = 1.0f / (4.0f * PI * R * R);
+  pdf = 1.0f / (PI * R * R);
   const float r = std::sqrt(u);
   const float theta = 2.0f * PI * v;
   return Vec2(r * std::cos(theta), r * std::sin(theta));
