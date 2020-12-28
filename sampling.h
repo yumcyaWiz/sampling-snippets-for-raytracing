73a49359c8e7566c7fa085df1e2914f501847fed
yumcyawiz <hashdolf1923@gmail.com>
2020-12-29 03:21:15 +0900

    impl sampleSphere

--- sampling.h
+++ sampling.h
@@ -1,5 +1,6 @@
 #ifndef _SAMPLING_H
 #define _SAMPLING_H
+#include <algorithm>
 #include <cmath>
 
 #include "constant.h"
@@ -11,6 +12,15 @@ inline Vec2 sampleCircle(float u, float v, float R, float& pdf) {
   const float r = std::sqrt(u);
   const float theta = 2.0f * PI * v;
   return Vec2(r * std::cos(theta), r * std::sin(theta));
+}
+
+// sample points on sphere
+inline Vec3 sampleSphere(float u, float v, float R, float& pdf) {
+  pdf = 1.0f / (4.0f * PI * R * R);
+  const float theta = std::acos(std::max(1.0f - 2.0f * u, 0.0f));
+  const float phi = 2.0f * PI * v;
+  return Vec3(R * std::cos(phi) * std::sin(theta), R * std::cos(theta),
+              R * std::sin(phi) * std::sin(theta));
 }
 
 #endif
