43e91dc4e3a57133296ab4f6d214cf13080cd324
yumcyawiz <hashdolf1923@gmail.com>
2020-12-29 03:26:31 +0900

    impl sampleHemisphere

--- sampling.h
+++ sampling.h
@@ -6,7 +6,7 @@
 #include "constant.h"
 #include "vec.h"
 
-// sample point on circle
+// sample point on circle uniformly
 inline Vec2 sampleCircle(float u, float v, float R, float& pdf) {
   pdf = 1.0f / (PI * R * R);
   const float r = std::sqrt(u);
@@ -14,10 +14,19 @@ inline Vec2 sampleCircle(float u, float v, float R, float& pdf) {
   return Vec2(r * std::cos(theta), r * std::sin(theta));
 }
 
-// sample points on sphere
+// sample points on sphere uniformly
 inline Vec3 sampleSphere(float u, float v, float R, float& pdf) {
   pdf = 1.0f / (4.0f * PI * R * R);
   const float theta = std::acos(std::max(1.0f - 2.0f * u, 0.0f));
+  const float phi = 2.0f * PI * v;
+  return Vec3(R * std::cos(phi) * std::sin(theta), R * std::cos(theta),
+              R * std::sin(phi) * std::sin(theta));
+}
+
+// sample points on hemisphere uniformly
+inline Vec3 sampleHemisphere(float u, float v, float R, float& pdf) {
+  pdf = 1.0f / (2.0f * PI * R * R);
+  const float theta = std::acos(std::max(1.0f - u, 0.0f));
   const float phi = 2.0f * PI * v;
   return Vec3(R * std::cos(phi) * std::sin(theta), R * std::cos(theta),
               R * std::sin(phi) * std::sin(theta));
